f42824cfeb37fca85bf87f212d8cc688e40c50dd
cdvines@iu.edu <cdvines623@gmail.com>
2019-04-18 18:10:22 -0400

    trying to use object of links

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 // User code here
-
-// HINT: start here: https://swapi.co/api/films/
-// User code here
-
 // HINT: start here: https://swapi.co/api/films/
 
 (function() {
+
+    var apiLinks = {
+        "films": "https://swapi.co/api/films/",
+        "people": "https://swapi.co/api/people/",
+        "planets": "https://swapi.co/api/planets/",
+        "species": "https://swapi.co/api/species/",
+        "starships": "https://swapi.co/api/starships/",
+        "vehicles": "https://swapi.co/api/vehicles/"
+    }
     
     $(document).ready(init('https://swapi.co/api/films/'))
-
     var reload = 1;
     
     function init(link) {
         
         // Create a request variable and assign a new XMLHttpRequest object to it.
-        var request = new XMLHttpRequest()
+        var request = new XMLHttpRequest();
 
         // Open a new connection, using the GET request on the URL endpoint
         request.open('GET', link, true)
@@ -35,6 +39,7 @@
         request.send();
     }
 
+
     function main(swapi) {
         if (reload == 1) {
             populateDropdown(swapi);
@@ -42,11 +47,11 @@
             // prevents multiple copies of the titles in the dropdown menu
         }
 
-        
+        $("li").mousedown(function() {
+            console.log(this);
+            populateTable(swapi, (this.getAttribute("id")));
+        });
 
-
-
-        populateTable(swapi);
     }
 
 
@@ -63,11 +68,8 @@
             // Set the href to the index of the film in results to reference its characters later.
             // Then set the li tag's inner text to the link
             link.innerHTML = film;
-            // console.log(link);
             link.setAttribute("href", "#");
-            link.setAttribute("i", i);
-            // listItem.setAttribute("onclick", "getCharacters()");
-            // console.log(listItem);
+            listItem.setAttribute("id", i);
             listItem.appendChild(link);
 
             // Then put the line of code in the dropdown
@@ -75,16 +77,50 @@
         }
     }
 
-
-
-    function getCharacters(swapi, index) {
-        console.log("in get characters with swap and " + index);
-    }
-
     function populateTable(swapi, index) {
 
-        for (var i in swapi.results[index]) {
-            console.log(i.characters);
+        console.log("clicked");
+        console.log(index);
+
+        // $("tbody").innerHTML = ""; // this will clear the table each time.
+
+        for (var i = 0; i < swapi.results[index].characters.length; i++) {
+
+            const tr = document.createElement('tr');
+            const tdName = document.createElement('td');
+            const tdShips = document.createElement('td');
+            const a = document.createElement('a');
+            const xIcon = document.createElement('span');
+            xIcon.setAttribute("class", "glyphicon glyphicon-remove");
+
+            var person;
+            var ref = new XMLHttpRequest();
+            ref.open("GET", swapi.results[index].characters[i], true);
+            ref.onload = function() {
+                // Begin accessing JSON data here
+                var character = JSON.parse(this.response);
+                person = character;
+                if (ref.status >= 200 && ref.status < 400) { // not exactly sure why the status need to be between 200 and 400
+                    tdName = character.name + " " + a.appendChild(xIcon);
+                    for (var i in character.vehicles) {
+                        console.log("another vehicle");
+                    }
+
+                } else {
+                    console.log("error");
+                }
+            };
+
+            // Then send request
+            ref.send();
+
+            
+
+            tdName = person.name;
+            console.log(tdName);
+
+            $("tbody").append(tr.appendChild(tdName && tdShips));
+
         }
 
         // for (var i = 0; i < swapi.results.length; i++) {
@@ -116,8 +152,8 @@
                 // console.log(swapi.results[i].characters[j].name);
                 // this is getting undefined each time
 
-            }
-        }
+        //     }
+        // }
     }
 
 
