31a60a32dcdc466896e42d8218451e901c0a8f0c
cdvines@iu.edu <cdvines623@gmail.com>
2019-04-22 12:53:24 -0400

    update

--- js/test/main-test.js
+++ js/test/main-test.js
@@ -1,9 +1,11 @@
-(function() {
+function square(x) { return x * x;}
+// var square = function() {}
+QUnit.test("square exits", function(assert) {
+    assert.ok(square, "Something called square exists");
+});
 
-    alert("Main tests");
+QUnit.test("square is a function", function(assert) {
+    assert.ok(typeof square == 'function', "square is of the type function");
+});
 
-    QUnit.test( "hello test", function( assert ) {
-        assert.ok( 1 == "1", "Passed!" );
-    });
 
-})();
