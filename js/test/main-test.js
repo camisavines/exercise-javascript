03debaeb41e9d58c2983f29cf9e3857fa2a5de91
cdvines@iu.edu <cdvines623@gmail.com>
2019-04-27 09:24:34 -0400

    no longer doing the tests

--- js/test/main-test.js
+++ js/test/main-test.js
@@ -1,47 +1,55 @@
-var swapi = $.getJSON("https://swapi.co/api/films/", function(result) {
-    return result;
-});
+// window.onload = function() {
 
-function populateDropdown(swapi) {
-    $(".myMenu").empty();
-    var goodResult = 0;
-    for (var i = 0; i < swapi.results.length; i++) {
+//     var swapi = $.getJSON("https://swapi.co/api/films/", function(p) {
+//         return p;
+//     });
 
-        //Make a quick reference to the current film
-        var film = swapi.results[i].title;
+//     function populateDropdown(swapi) {
+//         $(".myMenu").empty();
+//         var goodResult = 0;
+//         for (var i = 0; i < swapi.results.length; i++) {
 
-        // Create 2 constants for the list item and the link inside
-        const listItem = document.createElement('li');
-        const link = document.createElement('a');
+//             //Make a quick reference to the current film
+//             var film = swapi.results[i].title;
 
-        // Set the href to the index of the film in results to reference its characters later.
-        // Then set the li tag's inner text to the link
-        link.innerHTML = film;
-        link.setAttribute("href", "#");
-        listItem.setAttribute("id", i);
-        listItem.appendChild(link);
+//             // Create 2 constants for the list item and the link inside
+//             const listItem = document.createElement('li');
+//             const link = document.createElement('a');
 
-        // Then put the line of code in the dropdown
-        $(".myMenu").append(listItem);
-        goodResult++;
-    }
+//             // Set the href to the index of the film in results to reference its characters later.
+//             // Then set the li tag's inner text to the link
+//             link.innerHTML = film;
+//             link.setAttribute("href", "#");
+//             listItem.setAttribute("id", i);
+//             listItem.appendChild(link);
 
-    return 1;
-}
+//             // Then put the line of code in the dropdown
+//             $(".myMenu").append(listItem);
+//             goodResult++;
+//         }
+
+//         return 1;
+//     }
+
+
+// QUnit.test("populateDropdown", function(assert) {
+//     assert.ok(populateDropdown, "populateDropdown function exists");
+// });
+
+// QUnit.test("type of populateDropdown", function(assert) {
+//     assert.ok(typeof populateDropdown == 'function', "populateDropdown is of the type function");
+// });
+
+// QUnit.test("populateDropdown works", function(assert) {
+//     assert.equal(1, populateDropdown(), "Passes!");
+// });
+
+
+// }
 
 
 
-QUnit.test("populateDropdown", function(assert) {
-    assert.ok(populateDropdown, "populateDropdown function exists");
-});
 
-QUnit.test("type of populateDropdown", function(assert) {
-    assert.ok(typeof populateDropdown == 'function', "populateDropdown is of the type function");
-});
-
-QUnit.test("populateDropdown works", function(assert) {
-    assert.equal(1, populateDropdown(), "Passes!");
-})
 
 
 
